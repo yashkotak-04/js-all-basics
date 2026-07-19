05dd51f7fb814470cd785af233e9ec72ceacd2df
Yash Kotak <kotakyash192@gmail.com>
2026-07-19 22:31:01 +0530

    04 operations

---
+++ 04_operations.js
@@ -0,0 +1,45 @@
+// Operations :
+
+let value = 5 
+let negativevalue = -value
+console.log(negativevalue)  // -5
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)  --> power opearator
+// console.log(2%2)   --> modulus .
+
+let str1 = "hello"
+let str2 = " hello1"
+
+
+let str3 = str1 + str2  // here the two strings are concatenated and the result is a string.
+console.log(str3)  // hellohello1
+
+
+
+console.log("1" + 2)  //output is 12  --> here the number is converted into string and then concatenated with the string.
+console.log(1 + "2")  //output is 12
+
+
+
+console.log("1" + 2 + 2)  //output is 122  --> here the number is converted into string and then concatenated with the string.  [ string + number ]
+console.log(1 + 2 + "2")  //output is 32  --> here the number is converted into string and then concatenated with the string. [ number + string ]
+
+
+console.log((3 + 4) * (5 % 3));
+console.log("the value of true is " , +true);
+console.log(+"")   // output of the code is 0 .
+
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter)  // output is 101  --> here the value of gamecounter is incremented by 1.
+
+
+let gamecounter1 = 100
+++gamecounter1
+console.log(gamecounter)  // output is 101  --> here the value of gamecounter is incremented by 1.
+
