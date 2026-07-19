c7273a6c9b2a015bef59327984f9c4c0d440ab8c
Yash Kotak <kotakyash192@gmail.com>
2026-07-19 21:23:03 +0530

    02_datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"  // treat all JS code as newer version .
+
+//alert("Hello Aayushh")  // we can use alert to show the message in the pop up box.
+
+// alert(2+2)
+// alert(2*2)
+// alert(2/2)
+// alert(2-2)
+
+// alert(2%2)  // modulus operator gives the remainder of the division.
+// alert(2**2)  // exponent operator gives the power of the number.
+
+
+// When we can use the spacing ??
+// When we can user the semicolon ?? and etc ...
+
+// so , we have to make the code more readable and understandable for the other developers who will read our code in future. So , we can use the spacing and semicolon to make the code more readable and understandable.
+
+
+// like for exampple : code readablitiy : Example .
+
+console.log("Hello Aayushh"); console.log("How are you?")
+// this is the not that much readable . 
+
+
+let name = "Aayushh"; 
+
+
+// make me the code readable code .
+
+
+let name1 = null
+
+console.log(typeof name1)  // null is an object in JS.
