498504f9668d45c22233fe5f08b9a47a5c0191a2
Yash Kotak <kotakyash192@gmail.com>
2026-07-19 20:27:09 +0530

    01_variables file

---
+++ 01_variables.js
@@ -0,0 +1,32 @@
+const accountId = 12345;
+
+let accountEmail = "aayushh@gmail.com"
+var accountPassword = "12345"
+let accountstate;
+let accountstate1 = null;
+
+accountCity = "Bangalore"
+
+// accountId = 2   --> Not allowed .
+
+console.log(accountId)
+
+accountEmail = "zts@gmail.com"
+accountPassword = "123456"
+
+console.log(accountEmail)
+
+console.table([accountEmail, accountPassword, accountCity, accountstate, accountstate1])
+
+
+// as there is the difference between var, let and const. So we can use them according to our need.
+// So tell me the difference between them.
+// const is used to declare a variable that cannot be reassigned. It is block-scoped and must be initialized at the time of declaration.
+
+//Let : 
+// let is used to declare a variable that can be reassigned. It is block-scoped.
+
+//Var :
+// var is used to declare a variable that can be reassigned. It is function-scoped.
+// In general, it is recommended to use const and let instead of var, as they provide better scoping and help prevent accidental variable reassignments.
+
